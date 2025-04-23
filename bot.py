3d29a81d33959fc2d8d7bed17da015f44d53d8fb
Ruslan Samoilov <samoylov-rus@yandex-team.ru>
2025-04-23 16:24:20 +0300

    Исправил os.environ.get()

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
-app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
-
 import os
-from postgrest import SyncClient
+import requests
 
-SUPABASE_URL = os.environ.get("https://rwdkutiafmpuvigvnoiu.supabase.co") + "/rest/v1"
-SUPABASE_KEY = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZGt1dGlhZm1wdXZpZ3Zub2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MDA5MDEsImV4cCI6MjA2MDk3NjkwMX0.oFH5lEyqMWXO9ebk7ODeabSBHeEW-AGWJ6a2VJqW-n8")
-TELEGRAM_BOT_TOKEN = os.environ.get("7454880277:AAEXrtwlCJFLjXBnqXs7lPS8-0jgPbUKv-s")
+SUPABASE_URL = os.environ.get("SUPABASE_URL") + "/rest/v1/user_activity"
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+TELEGRAM_BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-supabase = SyncClient(SUPABASE_URL, headers={
+HEADERS = {
     "apikey": SUPABASE_KEY,
-    "Authorization": f"Bearer {SUPABASE_KEY}"
-})
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+    "Content-Type": "application/json",
+    "Prefer": "return=representation"
+}
