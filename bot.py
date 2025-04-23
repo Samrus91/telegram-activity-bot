cc17b9841731643b21d3e1d8498051fc61164e36
Ruslan Samoilov <samoylov-rus@yandex-team.ru>
2025-04-23 15:59:45 +0300

    Совместимые версии supabase и httpx

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from supabase_py import create_client
+from supabase import create_client
 
 SUPABASE_URL = os.environ.get("https://rwdkutiafmpuvigvnoiu.supabase.co")
 SUPABASE_KEY = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3ZGt1dGlhZm1wdXZpZ3Zub2l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MDA5MDEsImV4cCI6MjA2MDk3NjkwMX0.oFH5lEyqMWXO9ebk7ODeabSBHeEW-AGWJ6a2VJqW-n8")
